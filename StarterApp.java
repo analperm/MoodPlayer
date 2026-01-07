fdf68c1b08e5406588d42b490912d4202ac28bea
analperm <angelina.alpermann@gmx.de>
2026-01-07 14:50:54 +0100

    Add files via upload

---
+++ StarterApp.java
@@ -0,0 +1,9 @@
+package de.hsrm.mi.enia.moodplayer;
+
+import de.hsrm.mi.enia.moodplayer.presentation.MoodPlayerGUI;
+
+public class StarterApp {
+    public static void main(String[] args) {
+        MoodPlayerGUI.main(args);
+    }
+}
